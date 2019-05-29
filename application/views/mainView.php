8f016b8cc2a962f7cdddfd7ed4640cac81c261db
dkotenko <dkotenko@student.unit.ua>
2019-05-29 18:26:57 +0300

    xgdfs

--- application/views/mainView.php
+++ application/views/mainView.php
@@ -1,3 +1,25 @@
+<div id="modal-signup">
+			<!-- <div id="success-signup">You successful sign up</div>
+			<div id="error-signin">You entered your username or password incorrectly</div> -->
+			<div class="modal-signup-content">
+                <div class="header-signup-modal">Sign Up</div>
+                <form class="form-signup-modal">
+                    <input class="input-signup-modal" required type="text" name="email" placeholder="Email">
+                    <input class="input-signup-modal" required type="text" name="username" placeholder="Username">
+                    <input class="input-signup-modal" required type="password" name="passwd1" placeholder="Password">
+                    <input class="input-signup-modal" required type="password" name="passwd2" placeholder="Password">
+                    <input class="submit-signup-modal" type="submit" value="Sign Up">
+                </form>
+			    <!-- <form id="form-signUp-id" class="form-signUp-modal">
+				    <input required type="text" name="newLogin" class="input-signUp-modal" placeholder="Login">
+				    <input required type="text" name="email" class="input-signUp-modal" placeholder="Email">
+				    <input required type="password" name="passwd1" class="input-signUp-modal" placeholder="Password">
+				    <input required type="password" name="passwd2" class="input-signUp-modal" placeholder="Password">
+				    <input required type="submit" name="submit" class="submit-signUp-modal" value="Sign Up">
+                </form> -->
+            </div>
+</div>
+
 <header>
     <div class="header-user"></div>
     <div class="header-logo">Camagru</div>
@@ -7,4 +29,18 @@
     </div>
 </header>
 <div class="photo-place">
+    <div class="photo">
+    </div>
+    <div class="photo">
+    </div>
+    <div class="photo">
+    </div>
+    <div class="photo">
+    </div>
+    <div class="photo">
+    </div>
+    <div class="photo">
+    </div>
+    <div class="photo">
+    </div>
 </div>
