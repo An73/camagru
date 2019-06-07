cd64a905c0e03e2da52a8534e6cad7d572a5d87f
dkotenko <dkotenko@student.unit.ua>
2019-06-07 16:49:00 +0300

    dgfdf

--- application/views/shotView.php
+++ application/views/shotView.php
@@ -1,8 +1,15 @@
 
 <div class="container">
-    
     <video autoplay='true' id="videoElement"></video>
     <canvas id="canvas-area"></canvas>
+</div>
+<div class="filter-btns">
+    <button id="filter-standart" class="filter-button">Standart</button>
+    <button id="filter-blur" class="filter-button">Blur</button>
+    <button id="filter-bandw" class="filter-button">B&W</button>
+    <button id="filter-inverse" class="filter-button">Inverse</button>
+    <button id="filter-bluefill" class="filter-button">Bluefill</button>
+    <button id="filter-noir" class="filter-button">Noir</button>
 </div>
 
 <div id="main-btns" class="container-btn">
@@ -14,6 +21,8 @@
     <button class='btn' id="publish-btn">Publish</button>
     <button class='btn' id="try-again-btn">Try Again</button>
 </div>
+
+
 
 <svg id='image' version="1.1" xmlns="http://www.w3.org/2000/svg">
     <defs>
@@ -33,6 +42,28 @@
             <feFuncR type="table" tableValues="1 0"/>
             <feFuncG type="table" tableValues="1 0"/>
             <feFuncB type="table" tableValues="1 0"/>
+            </feComponentTransfer>
+        </filter>
+
+        <filter id="bluefill" x="0%" y="0%" width="100%" height="100%">
+            <feFlood flood-color="blue" result="A"/>
+            <feColorMatrix type="matrix" in="SourceGraphic" result="B" values="1   0  0  0 0  
+                           0   1  0  0 0   
+                           0   0  1  0 0   
+                           1   1  1  0 0
+                  "/>
+            <feMerge>
+            <feMergeNode in="A"/>
+            <feMergeNode in="B"/>
+            </feMerge>
+        </filter>
+
+        <filter id="noir">
+            <feGaussianBlur stdDeviation="1.5"/>
+            <feComponentTransfer>
+            <feFuncR type="discrete" tableValues="0 .5 1 1"/>
+            <feFuncG type="discrete" tableValues="0 .5 1"/>
+            <feFuncB type="discrete" tableValues="0"/>
             </feComponentTransfer>
         </filter>
     </defs>
--- public/js/shot.js
+++ public/js/shot.js
@@ -7,6 +7,13 @@ let publishBtns = document.getElementById('publish-btns');
 
 let tryAgainBtn = document.getElementById('try-again-btn');
 
+let filterStandart = document.getElementById('filter-standart');
+let filterBlur = document.getElementById('filter-blur');
+let filterBW = document.getElementById('filter-bandw');
+let filterInverse = document.getElementById('filter-inverse');
+let filterBluefill = document.getElementById('filter-bluefill');
+let filterNoir = document.getElementById('filter-noir');
+
 if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(function (stream) {
@@ -39,6 +46,41 @@ tryAgainBtn.onclick = function() {
   mainBtns.style.display = 'flex';
   publishBtns.style.display = 'none';
   canvas.style.display = 'none';
+}
+
+filterStandart.onclick = function() {
+  this.blur();
+  filterAplly('');
+}
+
+filterBlur.onclick = function() {
+  this.blur();
+  filterAplly('#blurEffect');
+}
+
+filterBW.onclick = function() {
+  this.blur();
+  filterAplly('#blackandwhite');
+}
+
+filterInverse.onclick = function() {
+  this.blur();
+  filterAplly('#inverse');
+}
+
+filterBluefill.onclick = function() {
+  this.blur();
+  filterAplly('#bluefill');
+}
+
+filterNoir.onclick = function() {
+  this.blur();
+  filterAplly('#noir');
+}
+
+function filterAplly(filter) {
+  video.style.filter = 'url(' + filter + ')';
+  canvas.style.filter = 'url(' + filter + ')';
 }
 
 function sendImage(data) {
