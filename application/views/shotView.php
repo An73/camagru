2dc315ef299421e44fc92b85bcba554e65a41076
dkotenko <dkotenko@student.unit.ua>
2019-06-07 15:53:46 +0300

    fsdfa

--- application/views/shotView.php
+++ application/views/shotView.php
@@ -1,4 +1,41 @@
-<div>ShotView</div>
-<video autoplay='true' id="videoElement"></video>
+
+<div class="container">
+    
+    <video autoplay='true' id="videoElement"></video>
+    <canvas id="canvas-area"></canvas>
+</div>
+
+<div id="main-btns" class="container-btn">
+    <button class='btn' id="shot-btn">Shot</button>
+    <button class='btn' id="upload-img-btn">Upload Image</button>
+</div>
+
+<div id="publish-btns" class="container-btn">
+    <button class='btn' id="publish-btn">Publish</button>
+    <button class='btn' id="try-again-btn">Try Again</button>
+</div>
+
+<svg id='image' version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+        <filter id="blurEffect">
+            <feGaussianBlur stdDeviation="3"/>
+        </filter>
+
+        <filter id="blackandwhite">
+            <feColorMatrix values="0.3333 0.3333 0.3333 0 0
+                                 0.3333 0.3333 0.3333 0 0
+                                0.3333 0.3333 0.3333 0 0
+                                0      0      0      1 0"/>
+        </filter>
+
+        <filter id="inverse">
+            <feComponentTransfer>
+            <feFuncR type="table" tableValues="1 0"/>
+            <feFuncG type="table" tableValues="1 0"/>
+            <feFuncB type="table" tableValues="1 0"/>
+            </feComponentTransfer>
+        </filter>
+    </defs>
+</svg>
+
 <img src="">
-<canvas style="display:none;"></canvas>
--- public/js/shot.js
+++ public/js/shot.js
@@ -1,12 +1,57 @@
 
-var video = document.getElementById("videoElement");
+let video = document.getElementById("videoElement");
+let shotBtn = document.getElementById('shot-btn');
+let canvas = document.getElementById('canvas-area');
+let mainBtns = document.getElementById('main-btns');
+let publishBtns = document.getElementById('publish-btns');
+
+let tryAgainBtn = document.getElementById('try-again-btn');
 
 if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(function (stream) {
         video.srcObject = stream;
+
+
       })
       .catch(function (err0r) {
         console.log("Something went wrong!");
       });
-  }
+}
+
+shotBtn.onclick = function() {
+  this.blur();
+  video.style.display = 'none';
+  mainBtns.style.display = 'none';
+  publishBtns.style.display = 'flex';
+  canvas.style.display = 'block';
+  context = canvas.getContext('2d');
+  canvas.width = 500;
+  canvas.height = 375;
+  context.drawImage(video, 0, 0, 500, 375);
+  let img = canvas.toDataURL('image/png').replace('data:image/png;base64,', '');
+  console.log(img);
+}
+
+tryAgainBtn.onclick = function() {
+  this.blur();
+  video.style.display = 'block';
+  mainBtns.style.display = 'flex';
+  publishBtns.style.display = 'none';
+  canvas.style.display = 'none';
+}
+
+function sendImage(data) {
+  let request = new XMLHttpRequest();
+    request.open('POST', '/shot/publish', true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencode');
+    request.send(data);
+    request.onreadystatechange = function() {
+        if (request.readyState === 4) {
+            if (request.status === 200 && callFunction !== null) {
+                
+
+            }
+        }
+    };
+}
